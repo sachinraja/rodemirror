1628a8337351ab05eedf177836eb95525ffc2de5
Sachin Raja <sachinraja2349@gmail.com>
2021-08-02 13:30:26 -0700

    chore: update to latest version of postinstall-dev

--- .yarn/plugins/@yarnpkg/plugin-postinstall-dev.cjs
+++ .yarn/plugins/@yarnpkg/plugin-postinstall-dev.cjs
@@ -1,9 +1,8 @@
 /* eslint-disable */
-//prettier-ignore
 module.exports = {
 name: "@yarnpkg/plugin-postinstall-dev",
 factory: function (require) {
-var plugin=(()=>{var e=Object.create,t=Object.defineProperty;var s=Object.getOwnPropertyDescriptor;var a=Object.getOwnPropertyNames;var i=Object.getPrototypeOf,c=Object.prototype.hasOwnProperty;var u=l=>t(l,"__esModule",{value:!0});var f=l=>{if(typeof require!="undefined")return require(l);throw new Error('Dynamic require of "'+l+'" is not supported')};var g=(l,n)=>{for(var o in n)t(l,o,{get:n[o],enumerable:!0})},m=(l,n,o)=>{if(n&&typeof n=="object"||typeof n=="function")for(let r of a(n))!c.call(l,r)&&r!=="default"&&t(l,r,{get:()=>n[r],enumerable:!(o=s(n,r))||o.enumerable});return l},y=l=>m(u(t(l!=null?e(i(l)):{},"default",l&&l.__esModule&&"default"in l?{get:()=>l.default,enumerable:!0}:{value:l,enumerable:!0})),l);var k={};g(k,{default:()=>h});var p=y(f("child_process")),d={hooks:{afterAllInstalled(){(0,p.spawnSync)("yarn",["run","postinstallDev"])}}},h=d;return k;})();
+var plugin;(()=>{"use strict";var e={d:(r,t)=>{for(var o in t)e.o(t,o)&&!e.o(r,o)&&Object.defineProperty(r,o,{enumerable:!0,get:t[o]})},o:(e,r)=>Object.prototype.hasOwnProperty.call(e,r),r:e=>{"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})}},r={};e.r(r),e.d(r,{default:()=>o});const t=require("child_process"),o={hooks:{afterAllInstalled(){(0,t.spawnSync)("yarn",["run","postinstallDev"])}}};plugin=r})();
 return plugin;
 }
 };
