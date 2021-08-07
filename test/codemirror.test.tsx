6f04f7c4faa5d365917a6f1cc51667643c901447
Sachin Raja <sachinraja2349@gmail.com>
2021-08-06 23:15:39 -0700

    test: change value

--- test/codemirror.test.tsx
+++ test/codemirror.test.tsx
@@ -1,12 +1,12 @@
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import CodeMirror from 'rodemirror'
 
 describe('component', () => {
   it('renders', async () => {
     const { baseElement } = render(<CodeMirror />)
-    const editorComponent = baseElement.querySelector('.cm-editor')
+    const editorElement = baseElement.querySelector('.cm-editor')
 
-    expect(editorComponent).toBeInTheDocument()
+    expect(editorElement).toBeInTheDocument()
   })
 
   it('renders with value prop text', async () => {
@@ -14,5 +14,27 @@ describe('component', () => {
     const { getByText } = render(<CodeMirror value={defaultValue} />)
 
     expect(getByText(defaultValue)).toBeInTheDocument()
+  })
+
+  it('can change value', async () => {
+    const defaultValue = 'hello'
+
+    const { baseElement, queryByText } = render(
+      <CodeMirror value={defaultValue} />
+    )
+
+    const editorElement = baseElement.querySelector('.cm-editor')
+
+    if (!editorElement) return
+
+    const secondValue = 'world'
+
+    // must use blur here because editor is contenteditable
+    fireEvent.blur(editorElement, {
+      target: { textContent: secondValue },
+    })
+
+    expect(queryByText(defaultValue)).not.toBeInTheDocument()
+    expect(queryByText(secondValue)).toBeInTheDocument()
   })
 })
