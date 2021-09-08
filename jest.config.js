a32af27b88c559ba34dfef2bd6e6dedc302f9e21
Sachin Raja <sachinraja2349@gmail.com>
2021-09-07 18:53:44 -0700

    fix: transpile to commonjs

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,12 @@
 export default {
   transformIgnorePatterns: ['node_modules', 'dist'],
   transform: {
-    '^.+\\.(j|t)sx?$': '@swc/jest',
+    '^.+\\.(j|t)sx?$': [
+      '@swc/jest',
+      {
+        module: { type: 'commonjs' },
+      },
+    ],
   },
   moduleNameMapper: {
     rodemirror: '<rootDir>/src/index.tsx',
