648854d0826d6933d57d602e90631acd87d049af
Sachin Raja <sachinraja2349@gmail.com>
2021-12-30 01:09:27 -0800

    test: switch to vitest

---
+++ test/basic.test.tsx
@@ -0,0 +1,38 @@
+import '@testing-library/jest-dom'
+import { render, fireEvent, screen } from '@testing-library/react'
+import CodeMirror from '../src'
+
+const defaultValue = 'hello'
+
+describe('component', () => {
+  it('renders', () => {
+    const { baseElement } = render(<CodeMirror />)
+    const editorElement = baseElement.querySelector('.cm-editor')
+
+    expect(editorElement).toBeInTheDocument()
+  })
+
+  it('renders with value prop text', () => {
+    render(<CodeMirror value={defaultValue} />)
+
+    expect(screen.getByText(defaultValue)).toBeInTheDocument()
+  })
+
+  it('can change value', () => {
+    const { baseElement } = render(<CodeMirror value={defaultValue} />)
+
+    const editorElement = baseElement.querySelector('.cm-editor')
+
+    if (!editorElement) return
+
+    const secondValue = 'world'
+
+    // must use blur here because editor is contenteditable
+    fireEvent.blur(editorElement, {
+      target: { textContent: secondValue },
+    })
+
+    expect(screen.queryByText(defaultValue)).not.toBeInTheDocument()
+    expect(screen.queryByText(secondValue)).toBeInTheDocument()
+  })
+})
