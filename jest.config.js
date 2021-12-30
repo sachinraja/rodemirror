502a3d415a450bfc8b36da48bccbe14194d3bd7e
Sachin Raja <sachinraja2349@gmail.com>
2021-12-30 01:10:15 -0800

    test: cleanup unneeded files

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-export default {
-  transformIgnorePatterns: ['node_modules', 'dist'],
-  transform: {
-    '^.+\\.(j|t)sx?$': ['@swc/jest'],
-  },
-  extensionsToTreatAsEsm: ['.ts', '.tsx'],
-  testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['@testing-library/jest-dom'],
-}
